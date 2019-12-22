741161406de801f5c4741c4c88fb938b5636245d
jefferson <jefferson.mello8@hotmail.com>
2019-12-22 16:28:15 -0300

    refatorei o cenario usando table para realizar as verificações com menos código e tudo de uma vez

--- features/step_definitions/restaurante_step.rb
+++ features/step_definitions/restaurante_step.rb
@@ -1,40 +1,17 @@
-Dado("que tenho uma lista de restaurantes") do
-  @restaurant_data = [
-    { name: "Bread & Bakery", category: "Padaria", delivery_time: "25 minutos", rating: "4.9" },
-    { name: "Burguer House", category: "Hamburgers", delivery_time: "30 minutos", rating: "3.5" },
-  ]
+Dado("que temos o seguintes restaurantes") do |table|
+  @restaurant_data = table.hashes
 end
 
 Quando("acesso a lista de restaurantes") do
   visit "/restaurants"
 end
 
-Então("vejo todas as opções disponíveis") do
+Então("devo ver todos os restaurantes desta lista") do
   restaurants = all(".restaurant-item")
-
-  expect(restaurants.size).to be > 0
-end
-
-Então("cada restaurante deve exibir sua categoria") do
-  restaurants = all(".restaurant-item")
-
   @restaurant_data.each_with_index do |value, index|
-    expect(restaurants[index]).to have_text value[:category]
-  end
-end
-
-Então("cada restaurante deve exibir o tempo de entrega") do
-  restaurants = all(".restaurant-item")
-
-  @restaurant_data.each_with_index do |value, index|
-    expect(restaurants[index]).to have_text value[:delivery_time]
-  end
-end
-
-Então("cada restaurante deve exibir sua nota de avaliação") do
-  restaurants = all(".restaurant-item")
-
-  @restaurant_data.each_with_index do |value, index|
-    expect(restaurants[index]).to have_text value[:rating]
+    expect(restaurants[index]).to have_text value["nome"].upcase
+    expect(restaurants[index]).to have_text value["categoria"]
+    expect(restaurants[index]).to have_text value["entrega"]
+    expect(restaurants[index]).to have_text value["avaliacao"]
   end
 end
